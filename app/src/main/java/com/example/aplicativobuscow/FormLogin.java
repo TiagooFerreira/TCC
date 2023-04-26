da39c773479b37b4581afcefe3c645c6f262bc02
Tiago Ferreira <tiagogoferreira14@gmail.com>
2023-04-26 14:14:27 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/aplicativobuscow/FormLogin.java
@@ -0,0 +1,34 @@
+package com.example.aplicativobuscow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+
+        getSupportActionBar().hide();
+        iniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormLogin.this,FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void iniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
+}
