941a842dc73a84d8cfa16e05a88d0b094d8233b1
Tiago Ferreira <tiagogoferreira14@gmail.com>
2023-05-09 15:24:36 -0300

    Configurando a maskara para EditText

--- app/src/main/java/com/example/aplicativobuscow/FormCadastro.java
+++ app/src/main/java/com/example/aplicativobuscow/FormCadastro.java
@@ -3,9 +3,14 @@ package com.example.aplicativobuscow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.aplicativobuscow.databinding.ActivityMainBinding;
 
 public class FormCadastro extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
