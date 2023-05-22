cc7aadd1524eeff6fb7d416ca371c4a69418f2b0
Tiago Ferreira <tiagogoferreira14@gmail.com>
2023-05-22 13:03:41 -0300

    Criando Caixa para ocultar ou mostrar senha

--- app/src/main/java/com/example/aplicativobuscow/FormCadastro.java
+++ app/src/main/java/com/example/aplicativobuscow/FormCadastro.java
@@ -3,21 +3,13 @@ package com.example.aplicativobuscow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import com.example.aplicativobuscow.databinding.ActivityFormCadastroBinding;
-import com.example.aplicativobuscow.databinding.ActivityMainBinding;
 
 public class FormCadastro extends AppCompatActivity {
 
-    private ActivityFormCadastroBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityFormCadastroBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_form_cadastro);
 
         getSupportActionBar().hide();
 
--- app/src/main/java/com/example/aplicativobuscow/FormLogin.java
+++ app/src/main/java/com/example/aplicativobuscow/FormLogin.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
@@ -13,6 +17,8 @@ public class FormLogin extends AppCompatActivity {
     private TextView text_tela_cadastro;
     private Button bt_entrar;
 
+    private EditText edit_senha;
+    private CheckBox checkBoxSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,22 @@ public class FormLogin extends AppCompatActivity {
 
         getSupportActionBar().hide();
         iniciarComponentes();
+
+        edit_senha = findViewById(R.id.edit_senha);
+        checkBoxSenha = findViewById(R.id.checkBoxSenha);
+
+        checkBoxSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    //mostrar senha
+                    edit_senha.setTransformationMethod(null);
+                }else{
+                    edit_senha.setTransformationMethod(new PasswordTransformationMethod());
+                }
+            }
+        });
+
 
         text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
             @Override
